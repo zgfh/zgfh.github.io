bfc2b04eeed145c9fb60927dc4168e19d76d361a
Alan <zg.zhu@daocloud.io>
2018-11-27 00:51:48 +0800

    Update and rename demo.sparkr to demo.r

--- demo.r
+++ demo.r
@@ -1,6 +1,8 @@
-Sys.setenv(SPARK_HOME="/usr/local/spark/")
+if (nchar(Sys.getenv("SPARK_HOME")) < 1) {
+  Sys.setenv(SPARK_HOME = "/usr/local/spark/")
+}
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
-sparkR.session(master = "spark://csp-dev-1-1-csp-adapter:7077", sparkConfig = list(spark.driver.memory = "2g",spark.app.name="jupyter-sparkR"))
+sparkR.session(master = "spark://csp-dev-1-1-csp-adapter:7077", sparkConfig = list(spark.driver.memory = "2g",spark.app.name="jupyter-sparkR",spark.sql.catalogImplementation="hive",spark.yarn.keytab=Sys.getenv("KEYTAB"),spark.yarn.principal=Sys.getenv("PRINCIPAL")))
 # Queries can be expressed in HiveQL.
 sql("CREATE TABLE IF NOT EXISTS mapr(id INT, name STRING)")
 sql("LOAD DATA LOCAL INPATH '/usr/local/spark/examples/src/main/resources/kv1.txt' INTO TABLE mapr")
