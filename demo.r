3f166e3acdc87c1269bc1f5eb9acab22dfc00534
Alan <zg.zhu@daocloud.io>
2018-11-27 00:53:21 +0800

    Update demo.r

--- demo.r
+++ demo.r
@@ -4,8 +4,5 @@ if (nchar(Sys.getenv("SPARK_HOME")) < 1) {
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
 sparkR.session(master = "spark://csp-dev-1-1-csp-adapter:7077", sparkConfig = list(spark.driver.memory = "2g",spark.app.name="jupyter-sparkR",spark.sql.catalogImplementation="hive",spark.yarn.keytab=Sys.getenv("KEYTAB"),spark.yarn.principal=Sys.getenv("PRINCIPAL")))
 # Queries can be expressed in HiveQL.
-sql("CREATE TABLE IF NOT EXISTS mapr(id INT, name STRING)")
-sql("LOAD DATA LOCAL INPATH '/usr/local/spark/examples/src/main/resources/kv1.txt' INTO TABLE mapr")
-employeeCount <- sql("SELECT count(*) from mapr")
-# results is now a SparkDataFrame
-head(employeeCount)
+sql("show tables")
+
