c251fdf3fcfb8348e2894ad058be029b7d96b5be
Alan <zg.zhu@daocloud.io>
2020-09-27 23:36:09 +0800

    Update get-k8s.sh

--- get-k8s.sh
+++ get-k8s.sh
@@ -7,8 +7,11 @@ set -e
 # 从节点: curl -fsSL https://zgfh.github.io/get-k8s.sh -o get-k8s.sh && bash get-k8s.sh join <master_ip>     
 # 网络: kubectl apply -f https://zgfh.github.io//calico-v3.10.yaml
 
+# 如果只想初始化环境，手动通过kubeadm 安装 curl -fsSL https://zgfh.github.io/get-k8s.sh -o get-k8s.sh && bash get-k8s.sh init
+
 K8S_VERSION=${K8S_VERSION-"v1.19.2"}
 
+init_host(){
 # 系统配置
 setenforce 0 && sed -i '/^SELINUX=/c\SELINUX=disabled' /etc/selinux/config
 systemctl stop firewalld || echo "no firewalld"
@@ -36,8 +39,11 @@ docker version >/dev/null 2>&1 || (curl -fsSL https://get.docker.com -o get-dock
 kubelet --version >/dev/null 2>&1 ||(docker run --rm -v /tmp:/tmp daocloud.io/daocloud/kube_binary:${K8S_VERSION} sh -c 'cp -rf /app /tmp/k8s')
 kubelet --version >/dev/null 2>&1 ||(cd /tmp/k8s/;./install.sh)
 yum install -y socat ebtables ethtool conntrack-tools
+}
+
 
 master_install(){
+init_host
 # 安装k8s
 kubeadm --image-repository=daocloud.io/daocloud --kubernetes-version=${K8S_VERSION} init --pod-network-cidr=10.252.0.0/16 --token=vqheb6.vqheb6tdangerous
 
@@ -51,6 +57,7 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 }
 slave_install(){
+init_host
 kubeadm join $1:6443 --token=vqheb6.vqheb6tdangerous --discovery-token-unsafe-skip-ca-verification
 echo "you need run (master print this): kubeadm join <masterip> --token xxx "
 }
@@ -61,10 +68,20 @@ kubeadm reset -f
 
 }
 
+# 重置
+init_install(){
+init_host
+echo "now,you can install k8s: "
+echo "  kubeadm --image-repository=daocloud.io/daocloud --kubernetes-version=${K8S_VERSION} init --pod-network-cidr=10.252.0.0/16 "
+}
+
+
 if [ "$1" == "join" ];then
 slave_install $2
 elif [ "$1" == "reset" ];then
 reset_install
+elif [ "$1" == "init" ];then
+init_install
 else
 master_install
 fi
