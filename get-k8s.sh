a79adbccd22a1e9edd6a9999d98487ff5bdc819f
Alan <zg.zhu@daocloud.io>
2020-09-27 23:27:13 +0800

    Update get-k8s.sh

--- get-k8s.sh
+++ get-k8s.sh
@@ -34,7 +34,7 @@ EOF
 docker version >/dev/null 2>&1 || (curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh && systemctl start docker && systemctl enable docker)
 kubelet --version >/dev/null 2>&1 ||(docker run --rm -v /tmp:/tmp daocloud.io/daocloud/kube_binary:${K8S_VERSION} sh -c 'cp -rf /app /tmp/k8s')
 kubelet --version >/dev/null 2>&1 ||(cd /tmp/k8s/;./install.sh)
-yum install -y socat ebtables ethtool
+yum install -y socat ebtables ethtool conntrack-tools
 
 master_install(){
 # 安装k8s
