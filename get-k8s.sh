13f857f57911826530eecf5ea3c0b5e6f68e782e
Alan <zg.zhu@daocloud.io>
2022-10-19 13:23:19 +0800

    Update get-k8s.sh

--- get-k8s.sh
+++ get-k8s.sh
@@ -90,6 +90,48 @@ mkdir -p nerdctl ;cd nerdctl
 wget https://files.m.daocloud.io/github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-full-${NERDCTL_VERSION}-linux-amd64.tar.gz
 tar -zvxf nerdctl-full-${NERDCTL_VERSION}-linux-amd64.tar.gz
 mkdir -p /opt/cni/bin ;cp -f libexec/cni/* /opt/cni/bin/ ;cp bin/* /usr/local/bin/ ;cp lib/systemd/system/*.service /usr/lib/systemd/system/
+
+cat >/etc/containerd/config.toml<<-EOF
+version = 2
+root = "/var/lib/containerd"
+state = "/run/containerd"
+oom_score = 0
+
+[grpc]
+  max_recv_message_size = 16777216
+  max_send_message_size = 16777216
+
+[debug]
+  level = "info"
+
+[metrics]
+  address = ""
+  grpc_histogram = false
+
+[plugins]
+  [plugins."io.containerd.grpc.v1.cri"]
+    sandbox_image = "daocloud.io/daocloud/pause:3.8"
+    max_container_log_line_size = -1
+    [plugins."io.containerd.grpc.v1.cri".containerd]
+      default_runtime_name = "runc"
+      snapshotter = "overlayfs"
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+          runtime_type = "io.containerd.runc.v2"
+          runtime_engine = ""
+          runtime_root = ""
+          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+            systemdCgroup = true
+    [plugins."io.containerd.grpc.v1.cri".registry]
+      [plugins."io.containerd.grpc.v1.cri".registry.configs]
+        [plugins."io.containerd.grpc.v1.cri".registry.configs."xx.daocloud.io"]
+          [plugins."io.containerd.grpc.v1.cri".registry.configs."xx.daocloud.io".tls]
+            insecure_skip_verify = true
+      [plugins."io.containerd.grpc.v1.cri".registry.mirrors]
+        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]
+          endpoint = ["https://registry-1.docker.io"]
+EOF
+
 systemctl enable containerd ;systemctl start containerd --now
 systemctl enable buildkit;systemctl start buildkit --now
 
